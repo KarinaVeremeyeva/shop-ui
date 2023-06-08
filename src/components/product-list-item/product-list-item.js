5d136590537c0975a17c4e9b8f7a7c72f4d92c00
KarinaVeremeyeva <57318127+KarinaVeremeyeva@users.noreply.github.com>
2023-06-08 14:54:26 +0300

    Make a product card clickable

--- src/components/product-list-item/product-list-item.js
+++ src/components/product-list-item/product-list-item.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Button, Card, CardContent, Grid, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Grid, Typography, Tooltip, CardActionArea } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import classes from './product-list-item.module.css';
 import image from '../../assets/no-image.jpg';
@@ -12,19 +12,25 @@ const ProductListItem = ({ product, onClick }) => {
     return (
         <Grid item xs={12} md={4}>
             <Card classes={{ root: classes.productListItem }} square>
-                <CardContent>
-                    <Box src={photo} component="img" className={classes.itemImage} alt="item-image" />
-                    <Typography variant="h6">
-                        <Link to={`/products/${id}`} className={classes.link}>
-                            {name}
-                        </Link>
-                    </Typography>
-                    <Typography variant="h6">${price}</Typography>
-                    <Typography variant="body1">{categoryName}</Typography>
-                    <Button onClick={() => onClick(id)} variant="contained" startIcon={<ShoppingCartIcon />}>
-                        Buy
-                    </Button>
-                </CardContent>
+                <Link to={`/products/${id}`} className={classes.link}>
+                    <CardActionArea>
+                        <CardContent>
+                            <Box src={photo} component="img" className={classes.itemImage} alt="item-image" />
+                            <Tooltip title={name}>
+                                <Typography variant="h6" className={classes.productNameContainer}>
+                                    <Link to={`/products/${id}`} className={classes.link}>
+                                        {name}
+                                    </Link>
+                                </Typography>
+                            </Tooltip>
+                            <Typography variant="h6">${price}</Typography>
+                            <Typography variant="body1">{categoryName}</Typography>
+                            <Button onClick={() => onClick(id)} variant="contained" startIcon={<ShoppingCartIcon />}>
+                                Buy
+                            </Button>
+                        </CardContent>
+                    </CardActionArea>
+                </Link>
             </Card>
         </Grid>
     );
